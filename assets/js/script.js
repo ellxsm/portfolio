81262a5943e1653ed2daf87723135847c232f864
Ellis Oliveira <ellis.oliveirab@gmail.com>
2024-11-09 11:25:10 -0300

    Atualização no menu responsivo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("nav__toggle");
     const navList = document.getElementById("nav__list");
+    const navLink = document.querySelectorAll(".nav__list__link");
 
     hamburger.addEventListener("click", () => {
         navList.classList.toggle("active");
     });
+
+    navLink.forEach(link => {
+        link.addEventListener("click", () => {
+            navList.classList.remove("active");
+        })
+    })
 });
